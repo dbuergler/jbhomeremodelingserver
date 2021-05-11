32aeebdba5a3bcb391e0e2abc65f119f08f9a9b7
Daniel Buergler <daniel.jbuergler@gmail.com>
2021-05-10 20:26:13 -0400

    message

--- db.js
+++ db.js
@@ -2,16 +2,16 @@ const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: "postgres",
-    // dialectOptions: {
-    //     ssl: {
-    //         require: true,
-    //         rejectedUnauthorized: false,
-    //     }
     dialectOptions: {
         ssl: {
-            sslmode: 'require',
+            require: true,
             rejectedUnauthorized: false,
         }
+    // dialectOptions: {
+    //     ssl: {
+    //         sslmode: 'require',
+    //         rejectedUnauthorized: false,
+    //     }
     }   
 });
 
